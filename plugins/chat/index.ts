2808f15e5ea56db650760aa7a23f83a8e80c5a73
reaby <petri.jarvisalo@gmail.com>
2024-01-13 09:33:30 +0200

    fix

--- plugins/chat/index.ts
+++ plugins/chat/index.ts
@@ -29,7 +29,7 @@ export default class Chat extends Plugin {
         if (data[0] == 0) return;
         if (data[2].startsWith("/")) return;
         const player = await tmc.getPlayer(data[1]);
-        const nick = player.nickname.replaceAll(/\$[iwozs]/i, "");
+        const nick = player.nickname.replaceAll(/\$[iwozs]/ig, "");
         const text = data[2];
         const msg = `$fff${nick}$z$s$fff »$ff0 ${text}`;
         tmc.server.send("ChatSendServerMessage", msg);
