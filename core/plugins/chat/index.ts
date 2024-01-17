f7ed3e64a528d564dd337c5cf5f07d2c763d547e
reaby <petri.jarvisalo@gmail.com>
2024-01-17 11:22:07 +0200

    fixes for ingame chat

--- core/plugins/chat/index.ts
+++ core/plugins/chat/index.ts
@@ -30,7 +30,7 @@ export default class Chat extends Plugin {
         if (data[2].startsWith("/")) return;
         const player = await tmc.getPlayer(data[1]);
         const nick = player.nickname.replaceAll(/\$[iwozs]/ig, "");
-        const text = data[2];
+        const text = data[2].replaceAll(/\n|\r|\t/g, "").replace(/[$]$/, "").trim();
         const msg = `$fff${nick}$z$s$fff »$ff0 ${text}`;
         tmc.server.send("ChatSendServerMessage", msg);
         tmc.cli(msg);
