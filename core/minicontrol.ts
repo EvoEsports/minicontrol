ea1c91ea53f0dcb4cb7e396a49ecf01a9b9f90c9
reaby <petri.jarvisalo@gmail.com>
2024-06-16 04:12:02 +0300

    1.3.5

--- core/minicontrol.ts
+++ core/minicontrol.ts
@@ -30,7 +30,7 @@ class MiniControl {
      * The version of MiniControl.
      */
     readonly brand: string = "$n$o$eeeMINI$o$z$s$abccontrol$z$s¤white¤";
-    readonly version: string = "0.3.5-dev";
+    readonly version: string = "0.3.5";
     /**
      * The start time of MiniControl.
      */
