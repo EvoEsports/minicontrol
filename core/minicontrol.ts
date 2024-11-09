9c1accfb9912826b245f12012feb70cd0fa962df
Petri Järvisalo <petri.jarvisalo@gmail.com>
2024-11-09 20:21:43 +0200

    decline starting of minicontrol with too old node versions

--- core/minicontrol.ts
+++ core/minicontrol.ts
@@ -33,6 +33,7 @@ import fs from 'fs';
 import Plugin from './plugins/index';
 import path from 'path';
 import {DepGraph} from 'dependency-graph';
+import semver from 'semver';
 
 export interface GameStruct {
     Name: string;
@@ -348,6 +349,10 @@ class MiniControl {
         const port = Number.parseInt(process.env.XMLRPC_PORT || '5000');
         this.cli(`¤info¤Starting ¤white¤MINIcontrol ${this.version}`);
         this.cli(`¤info¤Using Node ¤white¤${process.version}`);
+        if (semver.gt("21.5.0", process.version)) {
+            this.cli("¤error¤Your Node version is too old. Must be atleast 21.5.0, please upgrade!");
+            process.exit(1);
+        }
         this.cli('¤info¤Connecting to Trackmania Dedicated server at ¤white¤' + (process.env.XMLRPC_HOST ?? '127.0.0.1') + ':' + port);
         const status = await this.server.connect(process.env.XMLRPC_HOST ?? '127.0.0.1', port);
         if (!status) {
