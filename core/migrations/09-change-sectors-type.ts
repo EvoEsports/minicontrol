1f02822e1890ba199a811c6683792ffecee679d1
Petri Järvisalo <petri.jarvisalo@gmail.com>
2025-05-01 21:30:20 +0300

    fixes the migrate

--- core/migrations/09-change-sectors-type.ts
+++ core/migrations/09-change-sectors-type.ts
@@ -3,10 +3,20 @@ import type { Migration } from '../../migrate';
 
 export const up: Migration = async ({ context: sequelize }) => {
     const queryInterface = sequelize.getQueryInterface();
+
+    // notice: we need to remove the index before changing the column type, and then re-add it after
+    // because possible bug on sqlite composite index with different types
+    await queryInterface.removeIndex('sectors', 'idx_sectors_mapUuid_login');
+
     await queryInterface.changeColumn("sectors", "jsonData", {
         type: DataTypes.TEXT,
         allowNull: false,
         defaultValue: '[]'
+    });
+
+    await queryInterface.addIndex('sectors', ['mapUuid', 'login'], {
+        name: 'idx_sectors_mapUuid_login',
+        unique: true
     });
 }
 
