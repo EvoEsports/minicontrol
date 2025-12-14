a1169b53ebd00bd64ec1e5eae63befbd3982b99e
Petri Järvisalo <petri.jarvisalo@gmail.com>
2025-12-14 04:56:01 +0200

    add confirmButton

--- core/ui/components/index.ts
+++ core/ui/components/index.ts
@@ -1,3 +1,5 @@
 import button from "./button";
+import confirmButton from "./confirmButton";
 
 tmc.ui.registerComponentHandler("Button", button);
+tmc.ui.registerComponentHandler("ConfirmButton", confirmButton);
---
+++ core/ui/components/template.ts
@@ -0,0 +1,20 @@
+import type { objMap } from "@core/ui/manialink";
+
+/**
+ * a basic template component that does nothing
+ *
+ * @param a attributes
+ * @param inner inner content
+ * @param obj object map
+ *
+ * @returns manialink replacement and script
+ */
+export default async (a: { [key: string]: any }, inner: string, obj: objMap) => {
+    const [width, height] = a.size.split(" ").map((v: string) => parseFloat(v));
+    const [posX, posY] = a.pos.split(" ").map((v: string) => parseFloat(v));
+
+    let replacement =`<label pos="${a.pos}" size="${width} ${height}" text="Template Component" />`;
+    let script= "";
+
+    return { replacement, script};
+}
